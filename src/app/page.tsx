9323635ca16c66ec68fb6dcd059f0e0f0aac5ebb
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-08-13 23:19:32 +0300

    modif 1

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Image from "next/image";
+import rocketdab from '../../public/img/rocketdab.png'
+
+const Home = (): React.JSX.Element => {
+  return (
+      <main className="max-w-4xl mx-auto">
+
+      </main>
+  );
+};
+
+export default Home;
