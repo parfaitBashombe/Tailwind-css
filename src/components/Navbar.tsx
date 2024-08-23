3034136a7d45eb9889083aeed12bf8f388b4905c
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-08-23 20:17:23 +0300

    modif 1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,3 +29,20 @@ const Navbar = (): JSX.Element => {
         <Link href="#contacts" className="hover:opacity-90">Contact Us</Link>
       </nav>
     </section>
+
+    <section onClick={() => onClosedMenu()} id="mobile-menu" className={`absolute top-0 bg-black w-full text-5xl flex-col justify-content-center origin-top animate-open-menu ${open? "flex": "hidden"}`}>
+      <button className="text-8xl self-end px-6">
+        &times;
+      </button>
+      <nav className="flex flex-col h-screen items-center py-8" aria-label="mobile">
+        <Link href="#hero" className="w-full text-center py-6 hover:opacity-90">Home</Link>
+        <Link href="#rockets" className="w-full text-center py-6 hover:opacity-90">Our Rockets</Link>
+        <Link href="#testimonials" className="w-full text-center py-6 hover:opacity-90">Testimonials</Link>
+        <Link href="#contacts" className="w-full text-center py-6 hover:opacity-90">Contact Us</Link>
+        <Link href="#footer" className="w-full text-center py-6 hover:opacity-90">Legal</Link>
+      </nav>
+    </section>
+  </header>;
+};
+
+export default Navbar;
