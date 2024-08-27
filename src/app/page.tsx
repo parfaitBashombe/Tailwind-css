a40250f61b519408e56080be2e3413a934908fb6
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-08-27 18:27:50 +0300

    modif 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,20 +17,4 @@ const Home = (): React.JSX.Element => {
             </h2>
 
             <p className="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
-              We are building rockets for the next century today. From the moon to Mars, Jupiter and beyond...
-            </p>
-
-            <p className="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
-              Think Acme Rockets.
-            </p>
-          </article>
-
-         <Image src={rocketdab} width={350} height={350} alt="Rocket Dab" />
-        </section>
-
-        <hr className="mx-auto bg-black dark:bg-white w-1/2" />  
-      </main>
-  );
-};
-
-export default Home;
+              We are building rockets for the next century today. From the 
